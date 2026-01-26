473de2a54a0a9e64adad5f72895fe304421b2ae1
sigurdhp06 <sigurdhp06@gmail.com>
2026-01-26 12:52:41 +0100

    Sigurd mulitplayer første push
    Stadig i gang med menumanager

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
